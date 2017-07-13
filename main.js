208c6a10ce4cbdca2dd9d8a1a8bbad1395eada91
anzcarroll <anzcarroll@gmail.com>
2017-07-13 17:53:12 -0400

    making progress! now how dropChip and switchPlayers in console.log

--- main.js
+++ main.js
@@ -8,69 +8,90 @@ var connectFour = {
     //designate if luke=player "a" or if darth=player "b"
     currentPlayer: 1,
     board: [
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0]
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}],
+        [{},{},{},{},{},{},{}]
         ],
         // trying to create a function within object for the loop
              // of board arrays to use within other funtions that would need the data
-    loopForBoard: function() {
+   //loopForBoard: function(i, j) {
+   //     for (var i = 0; i < connectFour.board.length; i++ ) {
+   //     for ( var j = 0; j < connectFour.board[i].length; j++ ){
+   //         console.log([i, j]);
+   //             }  
+    //        }
+//loopForBoard.map();
+//        }
+//    }
+//});
+
+
+            // }
+ 
+ //       }
+    // },
+//     directions: [
+//         { x: 0, y: 1  }, // North-South
+//         { x: 1, y: 0  }, // East-West
+//         { x: 1, y: 1  }, // Northeast-Southwest
+//         { x: 1, y: -1 }  // Southeast-Northwest
+//         ]
+
+// };
+
+// //
+    dropChip: function() {
+
+//// Loop through the array of arrays
         for (var i = 0; i < connectFour.board.length; i++ ) {
-    for ( var j = 0; j < connectFour.board[i].length; j++ ) {
-
-            }
-
-        }
-    },
-    directions: [
-        { x: 0, y: 1  }, // North-South
-        { x: 1, y: 0  }, // East-West
-        { x: 1, y: 1  }, // Northeast-Southwest
-        { x: 1, y: -1 }  // Southeast-Northwest
-        ]
-
-}
-
-//
-var dropChip = function() {
-
-// Loop through the array of arrays
-for (var i = 0; i < connectFour.board.length; i++ ) {
-    for ( var j = 0; j < connectFour.board[i].length; j++ ) {
-        // console.log(connectFour.board.length);
-// if the loops are set to index0 then start turn
-        if (connectFour.board[i][j]=== 0) {
+        for ( var j = 0; j < connectFour.board[i].length; j++ ) {
+            let cell = $(`#columns ${i}${j}`);
+            console.log(connectFour.board.length);
+        //return the ids for each column in loop as string locations
+//// if the loops are set to index0 then start turn
+      if (connectFour.board[i][j]=== 0) {
             connectFour.board[i][j] = connectFour.currentPlayer;
         //need to finish fxn--->connectFour.changeColor(); 
         //need to finish fxn--->connectFour.isVictorious();
-              }
-        }
-    }
-}
-                            //SWITCH PLAYERS
-//if player === a then a //if player !== a then b
-var switchPlayers = function() {
+        return (cell);
+                 }
+             }
+         }
+    },
 
-    if (this.turn === 1) {
-        this.turn = 2;
-        console.log(2);
-    } else {
-        this.turn = 1;
-    console.log(1);
+    clickLocation: function(column) {
+        if (board.gameOver !== false) {
+            var target = column;
         }
+         board.dropCoin(target);
+         board.switchPlayers();
+            },
+        
+ 
+//                            //SWITCH PLAYERS
+// //if player === a then a //if player !== a then b
+    switchPlayers: function() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+                //$('#').click.css('background-color', red);
+                console.log(2);
+        } else {
+            this.currentPlayer = 1;
+                //$('#').css('background-color', green);
+                }
+                console.log(1);
+        //need to callfunctionhere(this.currentPlayer);
+            }
     }
-    switchPlayers();
+connectFour.dropChip();
+connectFour.switchPlayers();
 });  
-    //need to callfunctionhere(this.turn);
-//         }
-// }
+// // colorChanger: function (){
 
-// colorChanger: function (){
-
-// }
+// // }
      
    
 
@@ -84,17 +105,6 @@ var switchPlayers = function() {
    
 
     
-//     //when userOneStarsGame clicked it needs to call fxn that drops chip
-    //THEN the game starts
-    //chip appears in column that is clicked
-        //create chip
-        //create function
-// }
-// countChipsInColumn: function(colorArray, row, col) {
-//     //take array of Chips in play for color/user x
-//     //take array of chips in play for color/user y
-//     //
-// }
 
                             //CHECKFORWINNER
 //isVictorious: function(chips, placedX, placedY) {
