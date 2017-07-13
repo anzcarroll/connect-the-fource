50b3c88025a43366f2ae4cba5ee317b758122ff5
anzcarroll <anzcarroll@gmail.com>
2017-07-13 14:53:19 -0400

    the switchPlayer function is working

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var connectFour = {
         //need array to let any function know what it is searching through
         //
     gameOver: false,
-    winner: "nobody",
-    currentPlayer: 'x',
+    //designate if luke=player "a" or if darth=player "b"
+    currentPlayer: 1,
     board: [
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
@@ -14,75 +14,66 @@ var connectFour = {
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0]
+        ],
+        // trying to create a function within object for the loop
+             // of board arrays to use within other funtions that would need the data
+    loopForBoard: function() {
+        for (var i = 0; i < connectFour.board.length; i++ ) {
+    for ( var j = 0; j < connectFour.board[i].length; j++ ) {
+
+            }
+
+        }
+    },
+    directions: [
+        { x: 0, y: 1  }, // North-South
+        { x: 1, y: 0  }, // East-West
+        { x: 1, y: 1  }, // Northeast-Southwest
+        { x: 1, y: -1 }  // Southeast-Northwest
         ]
+
 }
+
+//
+var dropChip = function() {
+
 // Loop through the array of arrays
 for (var i = 0; i < connectFour.board.length; i++ ) {
     for ( var j = 0; j < connectFour.board[i].length; j++ ) {
-        console.log(connectFour.board.length);
+        // console.log(connectFour.board.length);
+// if the loops are set to index0 then start turn
+        if (connectFour.board[i][j]=== 0) {
+            connectFour.board[i][j] = connectFour.currentPlayer;
+        //need to finish fxn--->connectFour.changeColor(); 
+        //need to finish fxn--->connectFour.isVictorious();
+              }
+        }
     }
-};
-//using the index as positions
-//figure out through the index within the array or arrays where the first chip would drop
-//(loop within  a loops)
-//create function to start game from first drop OR a start button
-// put this function inside a click event.
+}
+                            //SWITCH PLAYERS
+//if player === a then a //if player !== a then b
+var switchPlayers = function() {
 
+    if (this.turn === 1) {
+        this.turn = 2;
+        console.log(2);
+    } else {
+        this.turn = 1;
+    console.log(1);
+        }
+    }
+    switchPlayers();
+});  
+    //need to callfunctionhere(this.turn);
+//         }
+// }
 
+// colorChanger: function (){
 
-
-
-//  function startGame() {
-//     for (i = 0; i<board.length; i++) {
-//        for (j = 0; j<board.length; i++) {
-
-//}
-//     }
-
-//  }
+// }
      
    
-// function createChips(color, radius, index){
-//     var radius = 39;
-//     var index = 0;
-//     for (index = 0; index <21; index++) {   //make red chips
-//        createChips(green,radius, index); //undefined is radius parameter
-//     }
-//     for (index = 0; index < 42; index++) {  //make black chips
-//         createChips(red, radius, index++);
-//     }
-// }; 
-     
 
-
-
-
-
-  
-
-// $('query').click(function(){
-//     startGame();
-// });
-
-
-
-
-
-
-
-     
-//     addChip: function (event) {
-//         var $gameRows = $(".eachRow");
-//         var $gameColumns = $(".col-md-6");
-
-//         for (i = 0; i < $gameColumns.length; i++) {
-//         var currentColumnn = $gameRows.$gameColumns[i];
-//         $currentColumn.onClick = chipDropped(currentColumn);
-//         console.log("is running");
-//         }
-//     }
-//  }  
-//   });
 
 
 
@@ -91,12 +82,6 @@ for (var i = 0; i < connectFour.board.length; i++ ) {
 //     //function needs to start game
     //need array of columns in the game 
    
-
-
-// userOneStartsGame: function (event) {
-//         //this function triggers a click, we don't want the page to refresh
-//     event.preventDefault();
-//     //WHEN the user clicks USER1 
 
     
 //     //when userOneStarsGame clicked it needs to call fxn that drops chip
@@ -111,6 +96,11 @@ for (var i = 0; i < connectFour.board.length; i++ ) {
 //     //
 // }
 
+                            //CHECKFORWINNER
+//isVictorious: function(chips, placedX, placedY) {
+// checkDirections.
+
+//}
 
 
 
@@ -118,13 +108,12 @@ for (var i = 0; i < connectFour.board.length; i++ ) {
 // method: {
 // GameReset: function(array) {
 //     this.gameOver = false;
-//     this.winner = "nobody";
-//     this.currentPlayer = "x";
+//     this.currentPlayer = "a";
 //     for (var i = 0; i < this.board.length; i++ ) {
 //     for ( var j = 0; j < this.board[i].length; j++ ) {
-//         $('#resetGame').set.click(this.board[i],[j], 0);
+//         $('#resetGame').click(this.board[i],[j], 0);
 //             }
 //         }
 //     }
 // };
-});
+// });
