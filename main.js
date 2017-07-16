8b3ca8d2b9bd9a098cb205e563e50fdd8e7a1adb
anzcarroll <anzcarroll@gmail.com>
2017-07-16 12:51:38 -0400

    started on check for winner logic and worked on Parse Int function---unsuccessful, needs more work

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 $(function() {
     var connectFour = {
-        //need array to let any function know what it is searching through
         gameOver: false,
         //designate if luke=player 1 or if darth=player 2
         currentPlayer: 1,
-        //array for each square in the gameboard
+        //need array to let any function know what it is searching through
         board: [
             [{}, {}, {}, {}, {}, {}, {}],
             [{}, {}, {}, {}, {}, {}, {}],
@@ -23,8 +22,10 @@ $(function() {
                 var whereStartDrop = column.target;
                  //then the column is not available for palyer move
                 whereStartDrop === true;
+                console.log("cant drop here");
             } else {
                 //else the column is available for player move
+                console.log("can dro here");
                  whereStartDrop === false;   
             }
             //give var to the array of the columns now declared in new var
@@ -34,10 +35,12 @@ $(function() {
             // console.log('switchPlayersisworking');
             if (this.currentPlayer === 1) {
                 currentPlayer = this.currentPlayer = 2;
-                $('#turn').html('Darth Vader');
+                $('#turn').html('Luke Skywalker');
+                $('#turn').css('color', 'green');
             } else {
                 currentPlayer = this.currentPlayer = 1;
-                $('#turn').html('Luke Skywalker');
+                $('#turn').html('Darth Vader');
+                $('#turn').css('color', 'red')
             }
             return;
         }
@@ -45,7 +48,6 @@ $(function() {
     
     // @TODO: There is a bug that changes the user state when click is too fast
     $('.col-md-6').on('click', function(cell) {
-        
         currentClickedCell = parseInt(cell.target.id.split('-')[1]);
         nextCellId = currentClickedCell+10;
         cellBelow = $('#columns-' + nextCellId)[0];
@@ -53,7 +55,7 @@ $(function() {
             // console.log($(cellBelow)[0])
             // console.log($(cellBelow).hasClass('p1') || $(cellBelow).hasClass('p2'));
         }
-        // dont allow user to click any row < 4 on first try
+        // dont allow user to click any row < 4 on first try // allow to stack after one below
         if (currentClickedCell >= 50 || $(cellBelow).hasClass('p1') || $(cellBelow).hasClass('p2')) {
             connectFour.switchPlayers();
             connectFour.dropChip(cell.target);
@@ -71,7 +73,7 @@ $(function() {
         }
     });
 
-    //when its the players turn and they click on cell #, then the color changes
+    //when its the players turn and they click on cell #, then the color changes 
     function changeColors(currentPlayer, col) {
         console.log(currentPlayer);
         if (currentPlayer == 1) {
@@ -83,26 +85,63 @@ $(function() {
             col.addClass('red');
             col.addClass('p2');
         }
-
-        // connectFour.switchPlayers();
     }
+    //check win
+    function isVictorious(SOMETHINGHERE) {
+            checkHorizontal = function() {  
+                for (var i =0; i <connectFour.board.length; i++) {
+                    for (var j = 0; j <connectFour.board[0].length - 3; j++) {
+                var checkSum =         
+                    }
+                }
+            }
+            checkVertical = function() {
+
+            }
+            checkDiagonal = function() {
+
+            }
+            alertWinner = function() {
+
+            }
+     }
+
+    alert win
+    function alertWinner() {
+
+    }
+
+
+    // function clearBoard() {
+    //    connectFour.gameOver === false;
+    //     board = [
+    //         [{}, {}, {}, {}, {}, {}, {}],
+    //         [{}, {}, {}, {}, {}, {}, {}],
+    //         [{}, {}, {}, {}, {}, {}, {}],
+    //         [{}, {}, {}, {}, {}, {}, {}],
+    //         [{}, {}, {}, {}, {}, {}, {}],
+    //         [{}, {}, {}, {}, {}, {}, {}]
+        
+    // };
 
     // PUT EVERYTHING ^^^^HERE^^^^! OR TRY.
 
     //created function that changes "columns-__" to a number
     //need to split the number for x & y coordinates
     //use function to decalred wins at x,y coordinates
-    var parseIntColumnIds = function(x, y) {
+parseIntColumnIds = function(cell, x, y) {
         // console.log('i have changed your strings to numbers');
         for (var i = 0; i < 6; i++) {
             for (var j = 0; j < 7; j++) {
                 let cell = $(`#columns-${i}${j}`);
                 let cellId = parseInt(cell.attr('id').replace(/columns-/, ''));
-                // console.log(cell);
-                let x = parseInt(cellId);
-                let y = parseInt(cellId);
-                // console.log(x + ' im x ');
+                cellCoordinates = cellId + (cell.target.id.split('-')[1]);
+                let x = parseInt(cellCoordinates)[i];
+                
+                let y = parseInt(cellCoordinates)[j];
+                 console.log("im workingish");
             }
         }
     };
+
 });
