cc3c1bc1b72f5847c51efa2ca16f9fe5b0ee9480
anzcarroll <anzcarroll@gmail.com>
2017-07-17 12:48:32 -0400

    last  few changes before presentation

--- main.js
+++ main.js
@@ -18,48 +18,42 @@ $(function() {
             var columnToDrop = $('.col-md-6')[$('.col-md-6').length - 6];
             var whereStartDrop = false;
             //if the colum attepmting click has p1 || p2
-            if ($(columnToDrop).hasClass('p1') || $(columnToDrop).hasClass('p2')) {
-                var whereStartDrop = column.target;
-                 //then the column is not available for palyer move
-                whereStartDrop === true;
-                console.log("cant drop here");
-            } else {
+                if ($(columnToDrop).hasClass('p1') || $(columnToDrop).hasClass('p2')) {
+                    var whereStartDrop = column.target;
+                    //then the column is not available for palyer move
+                    whereStartDrop === true;
+                } else {   
                 //else the column is available for player move
-                console.log("can dro here");
                  whereStartDrop === false;   
-            }
-            //give var to the array of the columns now declared in new var
+                }
         },
 
         switchPlayers: function(currentPlayer) {
-            // console.log('switchPlayersisworking');
-            if (this.currentPlayer === 1) {
-                currentPlayer = this.currentPlayer = 2;
-                $('#turn').html('Luke Skywalker');
-                $('#turn').css('color', 'green');
-            } else {
-                currentPlayer = this.currentPlayer = 1;
-                $('#turn').html('Darth Vader');
-                $('#turn').css('color', 'red')
-            }
-            return;
+                //console.log('switchPlayersisworking');
+                if (this.currentPlayer === 1) {
+                    currentPlayer = this.currentPlayer = 2;
+                    $('#turn').html('Luke Skywalker');
+                    $('#turn').css('color', 'green');
+                } else {
+                    currentPlayer = this.currentPlayer = 1;
+                    $('#turn').html('Darth Vader');
+                    $('#turn').css('color', 'red')
+                }
+                return;
         }
     }; //<---bracket ends connectFour object
-    
+
     // @TODO: There is a bug that changes the user state when click is too fast
     $('.col-md-6').on('click', function(cell) {
         currentClickedCell = parseInt(cell.target.id.split('-')[1]);
         nextCellId = currentClickedCell+10;
         cellBelow = $('#columns-' + nextCellId)[0];
-        if (cellBelow !== undefined) {
-            // console.log($(cellBelow)[0])
-            // console.log($(cellBelow).hasClass('p1') || $(cellBelow).hasClass('p2'));
-        }
+            // if (cellBelow !== undefined) {
+        // }
         // dont allow user to click any row < 4 on first try // allow to stack after one below
         if (currentClickedCell >= 50 || $(cellBelow).hasClass('p1') || $(cellBelow).hasClass('p2')) {
             connectFour.switchPlayers();
             connectFour.dropChip(cell.target);
-            
             if (connectFour.currentPlayer == 1) {
                 if (!$(cell.target).hasClass('p2')) {
                     changeColors(connectFour.currentPlayer, $(this));
@@ -70,6 +64,7 @@ $(function() {
                     changeColors(connectFour.currentPlayer, $(this));
                 }
             }
+          isVictory();   
         }
     });
 
@@ -87,61 +82,98 @@ $(function() {
         }
     }
     //check win
-    function isVictorious(SOMETHINGHERE) {
-            checkHorizontal = function() {  
-                for (var i =0; i <connectFour.board.length; i++) {
-                    for (var j = 0; j <connectFour.board[0].length - 3; j++) {
-                var checkSum =         
+ function isVictory() {
+        var i;
+        var j;
+        var k;
+        var rTemp = 0;
+        var bTemp = 0;
+        var brTemp = 0;
+        var trTemp = 0;
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j < 7; j++) {
+            for (k = 0; k <= 3; k++) {
+                    //from (i,j) to right
+                    if (j + k < 7) {
+                        rTemp += connectFour.board[i][j + k]
+                    }
+                    //from (i,j) to bottom
+                    if (i + k < 6) {
+                        bTemp += connectFour.board[i + k][j];
+                    }
+
+                    //from (i,j) to bottom-right
+                    if (i + k < 6 && j + k < 7) {
+                        brTemp += connectFour.board[i + k][j + k]
+                    }
+
+                    //from (i,j) to top-right
+                    if (i - k >= 0 && j + k < 7) {
+                        trTemp += connectFour.board[i - k][j + k];
                     }
                 }
+                if (Math.abs(rTemp) === 4) {
+                    alertWinner(currentPlayer);
+                } else if (Math.abs(bTemp) === 4) {
+                    alertWinner(currentPlayer);
+                } else if (Math.abs(brTemp) === 4) {
+                    alertWinner(currentPlayer);
+                } else if (Math.abs(trTemp) === 4) {
+                    alertWinner(currentPlayer);
+                }
             }
-            checkVertical = function() {
-
-            }
-            checkDiagonal = function() {
-
-            }
-            alertWinner = function() {
-
-            }
-     }
-
-    alert win
-    function alertWinner() {
-
     }
+  
 
 
-    // function clearBoard() {
-    //    connectFour.gameOver === false;
-    //     board = [
-    //         [{}, {}, {}, {}, {}, {}, {}],
-    //         [{}, {}, {}, {}, {}, {}, {}],
-    //         [{}, {}, {}, {}, {}, {}, {}],
-    //         [{}, {}, {}, {}, {}, {}, {}],
-    //         [{}, {}, {}, {}, {}, {}, {}],
-    //         [{}, {}, {}, {}, {}, {}, {}]
-        
+     alertWinner = function(currentPlayer) {
+         connectFour.gameOver = true;
+        if (connectFour.currentPlayer === 1) {
+             alert("Victory for Darth!");
+         } else {
+             alert("Victory for Luke!");
+         }
+     }
+    
+    
+    // parseIntColumnIds = function(currentClickedCell) {
+    //    for (var i = 0; i < 6; i++) {
+    //        for (var j = 0; j < 7; j++) {
+    //     let cell = $(`#columns-${i}${j}${k}`);
+    //     let cellId = parseInt(cell.attr('id').replace(/columns-/, ''));
+    //             cellId += (currentClickedCell);
+    //             let x = cellId[i];
+    //             let y = cellId[j];
+    //               console.log(x, y);;
+    //         }
+    //     }
+    // }    
+
+    //  parseIntColumnIds(currentClickedCell);
+    //     }
     // };
 
-    // PUT EVERYTHING ^^^^HERE^^^^! OR TRY.
+    
+//FIGURE OUT WHY THE FUCK THIS ISNT WORKING
+
+
+    // function reloadPage(){
+    //     var resetButton = $('#resetGame');
+    //         if (connectFour.board === false) {
+    //         (connectFour.board === true)
+    //         }
+    //     resetButton.onclick= reloadPage;
+    //     window.location.reload(true);
+
+    // }
+    // reloadPage();
+        
+// PUT EVERYTHING ^^^^HERE^^^^! OR TRY.
 
     //created function that changes "columns-__" to a number
     //need to split the number for x & y coordinates
     //use function to decalred wins at x,y coordinates
-parseIntColumnIds = function(cell, x, y) {
-        // console.log('i have changed your strings to numbers');
-        for (var i = 0; i < 6; i++) {
-            for (var j = 0; j < 7; j++) {
-                let cell = $(`#columns-${i}${j}`);
-                let cellId = parseInt(cell.attr('id').replace(/columns-/, ''));
-                cellCoordinates = cellId + (cell.target.id.split('-')[1]);
-                let x = parseInt(cellCoordinates)[i];
-                
-                let y = parseInt(cellCoordinates)[j];
-                 console.log("im workingish");
-            }
-        }
-    };
 
+
+}
 });
